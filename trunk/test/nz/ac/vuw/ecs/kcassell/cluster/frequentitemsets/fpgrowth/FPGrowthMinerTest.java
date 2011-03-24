f775acd07477fd75fbb9d199f4d96ab533b2c313
kcassell@ecs.vuw.ac.nz <kcassell@ecs.vuw.ac.nz@bab8895e-7348-1769-9d02-01492452b86c>
2011-03-24 02:36:32 +0000

    Working on FPGrowth

--- trunk/test/nz/ac/vuw/ecs/kcassell/cluster/frequentitemsets/fpgrowth/FPGrowthMinerTest.java
+++ trunk/test/nz/ac/vuw/ecs/kcassell/cluster/frequentitemsets/fpgrowth/FPGrowthMinerTest.java
@@ -2,6 +2,7 @@ package nz.ac.vuw.ecs.kcassell.cluster.frequentitemsets.fpgrowth;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import junit.framework.TestCase;
 import nz.ac.vuw.ecs.kcassell.cluster.frequentitemsets.ItemSupportList;
@@ -9,6 +10,38 @@ import nz.ac.vuw.ecs.kcassell.cluster.frequentitemsets.ItemSupportList;
 import org.junit.Test;
 
 public class FPGrowthMinerTest extends TestCase {
+	
+	Collection<ItemSupportList> transactions = null;
+	ItemSupportList t1 = null;
+	ItemSupportList t2 = null;
+	ItemSupportList t3 = null;
+	ItemSupportList t4 = null;
+
+	public void setUp() {
+		transactions = new ArrayList<ItemSupportList>();
+		t1 = new ItemSupportList("client1", null, null);
+		transactions.add(t1);
+		
+		ArrayList<String> members2 = new ArrayList<String>();
+		members2.add("m1");
+		members2.add("m2");
+		t2 = new ItemSupportList("client1", members2, null);
+		transactions.add(t2);
+
+		ArrayList<String> members3 = new ArrayList<String>();
+		members3.add("m2");
+		members3.add("m3");
+		t3 = new ItemSupportList("client1", members3, null);
+		transactions.add(t3);
+
+		ArrayList<String> members4 = new ArrayList<String>();
+		members4.add("m1");
+		members4.add("m2");
+		members4.add("m3");
+		members4.add("m4");
+		t4 = new ItemSupportList("client1", members4, null);
+		transactions.add(t4);
+	}
 
 	@Test
 	public void testBuildFPTree() {
@@ -17,33 +50,44 @@ public class FPGrowthMinerTest extends TestCase {
 
 	@Test
 	public void testGetFrequentItems() {
-		Collection<ItemSupportList> transactions = new ArrayList<ItemSupportList>();
-		ItemSupportList t1 = new ItemSupportList("client1", null);
-		transactions.add(t1);
-		
-		ArrayList<String> members2 = new ArrayList<String>();
-		members2.add("m1");
-		members2.add("m2");
-		ItemSupportList t2 = new ItemSupportList("client1", members2);
-		transactions.add(t2);
-
-		ArrayList<String> members3 = new ArrayList<String>();
-		members3.add("m1");
-		members3.add("m3");
-		ItemSupportList t3 = new ItemSupportList("client1", members3);
-		transactions.add(t3);
-
-		ArrayList<String> members4 = new ArrayList<String>();
-		members4.add("m1");
-		members2.add("m2");
-		members2.add("m3");
-		members4.add("m4");
-		ItemSupportList t4 = new ItemSupportList("client1", members4);
-		transactions.add(t4);
-
 		FPGrowthMiner miner = new FPGrowthMiner();
 		ItemSupportList frequentItems = miner.getFrequentItems(transactions);
 		System.out.println("frequentItems = " + frequentItems);
+		List<String> members = frequentItems.getItems();
+		assertEquals("m2", members.get(0));
+		assertEquals(3.0, frequentItems.getSupport("m2"), 0.00001);
+		assertEquals("m1", members.get(1));
+		assertEquals(2.0, frequentItems.getSupport("m1"), 0.00001);
+		assertEquals("m3", members.get(2));
+		assertEquals(2.0, frequentItems.getSupport("m3"), 0.00001);
+		assertEquals("m4", members.get(3));
+		assertEquals(1.0, frequentItems.getSupport("m4"), 0.00001);
+	}
+	
+	public void testSortItems() {
+		FPGrowthMiner miner = new FPGrowthMiner();
+		ItemSupportList frequentItems = miner.getFrequentItems(transactions);
+/*		frequentItems = ItemSupportList
+			m2:	3.0
+			m1:	2.0
+			m3:	2.0
+			m4:	1.0		*/
+		miner.sortItems(t2);
+		List<String> members2 = t2.getItems();
+		assertEquals("m2", members2.get(0));
+		assertEquals("m1", members2.get(1));
+		
+		miner.sortItems(t3);
+		List<String> members3 = t3.getItems();
+		assertEquals("m2", members3.get(0));
+		assertEquals("m3", members3.get(1));
+		
+		miner.sortItems(t4);
+		List<String> members4 = t4.getItems();
+		assertEquals("m2", members4.get(0));
+		assertEquals("m1", members4.get(1));
+		assertEquals("m3", members4.get(2));
+		assertEquals("m4", members4.get(3));
 	}
 
 }

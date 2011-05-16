24308e93a0d5f539788c4c66617ca71f1336a421
kcassell@ecs.vuw.ac.nz <kcassell@ecs.vuw.ac.nz@bab8895e-7348-1769-9d02-01492452b86c>
2011-05-16 03:31:58 +0000

    Added some commented out code to serve as the basis for saving the table to a CSV string.

--- trunk/src/nz/ac/vuw/ecs/kcassell/callgraph/gui/MetricsTableModel.java
+++ trunk/src/nz/ac/vuw/ecs/kcassell/callgraph/gui/MetricsTableModel.java
@@ -109,5 +109,24 @@ class MetricsTableModel extends AbstractTableModel {
 	public void setHandles(String[] handles) {
 		this.handles = handles;
 	}
+	
+//	public void exportTable(JTable table, File file) throws IOException {
+//		TableModel model = table.getModel();
+//		FileWriter out = new FileWriter(file);
+////	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true));
+////	PrintWriter fileWriter = new PrintWriter(bufferedWriter);
+//
+//		for(int i=0; i < model.getColumnCount(); i++) {
+//	out.write(model.getColumnName(i) + "\t");
+//		}
+//		out.write("\n");
+//
+//		for(int i=0; i< model.getRowCount(); i++) {
+//	for(int j=0; j < model.getColumnCount(); j++) {
+//		out.write(model.getValueAt(i,j).toString()+"\t");
+//		}
+//		out.write("\n");
+//	}
+
 
 }
